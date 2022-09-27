a95a81b0a2f28528d125ad82d352d97620579c2f
artyomtrus <trusartyom@yandex.by>
2022-09-27 21:41:57 +0300

    пр 14 промежуточная

--- models/user.js
+++ models/user.js
@@ -16,6 +16,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     select: false,
+
   },
   name: {
     type: String,
@@ -35,6 +36,11 @@ const userSchema = new mongoose.Schema({
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 
+}, {
+  toObject: {
+    useProjection: true,
+    versionKey: false,
+  },
 });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
